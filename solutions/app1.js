f2e42bf0f2f7735d98cbbb24858f8ce060f7603b
zsuzsasovari <105643090+zsuzsasovari@users.noreply.github.com>
2022-08-31 13:17:13 +0200

    kész

--- solutions/app1.js
+++ solutions/app1.js
@@ -1,1 +1,7 @@
+const setCookie = (value) => {
+    let now = new Date(Date.now() + (15 * 60 * 1000));
+    let expires = now.toUTCString();
+    document.cookie =`token=${value};expires=${expires}`;
+}
 
+export default setCookie;
